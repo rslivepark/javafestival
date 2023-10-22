e43c5db1d4369cdfcab633b1ff2924a075174ab9
rslivepark <146307227+rslivepark@users.noreply.github.com>
2023-10-22 18:54:00 +0900

    Update Ex34.java

--- src/Ex34.java
+++ src/Ex34.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class Ex34 {
 	// 2진수 입력받아 10진수로 바꾸기 
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		System.out.print("2진수 입력 : ");
-		String num = sc.next();
-
-		int two = Integer.parseInt(num, 2);
-		System.out.println(two);
+		
+	 String str1 = "01001101";
+	 String str2 = "00101000";
+	 
+	 int toTen = Integer.parseInt(str1, 2);
+	 System.out.println(str1 + "(2) = "+ toTen + "(10)");
+	 toTen = Integer.parseInt(str2, 2);
+	 System.out.println(str2 + "(2) = "+ toTen + "(10)");
 
 	}
 }
