a5f4ad98beda3e805cdaa8c15871ec2e961f9988
rslivepark <146307227+rslivepark@users.noreply.github.com>
2023-10-22 19:21:19 +0900

    Update Ex01.java

--- src/Ex01.java
+++ src/Ex01.java
@@ -2,28 +2,21 @@ import java.util.Scanner;
 
 public class Ex01 {
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-		// 현재 몸무게 입력
 		System.out.print("현재몸무게 : ");
 		int now = sc.nextInt();
-
-		// 목표 몸무게 입력
 		System.out.print("목표몸무게 : ");
-		int future = sc.nextInt();
-		int i = 1;
-		int j = 0;
-
+		int goal = sc.nextInt();
+		int cnt = 1;
 		while (true) {
-			System.out.print(i + "주차 감량 몸무게 : ");
-			j = sc.nextInt();
-			now -= j;
-			if (future >= now) {
+			System.out.print(cnt + "주차 감량 몸무게 : ");
+			int lose = sc.nextInt();
+			now -= lose;
+			cnt++;
+			if(now<=goal) {
+				System.out.println(now+"kg 달성!! 축하합니다. ");
 				break;
-			} // if
-			i++;
-		} // while
-		System.out.println(now + "kg 달성! 축하합니다. ");
-
+			}//if
+		}//while
 	}
 }
