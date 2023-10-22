b809c3f45506abd80dc1572a9f67f7093201fdec
rslivepark <146307227+rslivepark@users.noreply.github.com>
2023-10-22 16:45:00 +0900

    Update Ex21.java

--- src/Ex21.java
+++ src/Ex21.java
@@ -1,17 +1,13 @@
+import java.util.*;
 
-import java.util.Arrays;
-import java.util.Random;
-
-public class Ex21 {
-//중복 없이 숫자 뽑는 로또 프로그램 만들기 
+public class Test {
+	// 중복없이 숫자 뽑는 로또 프로그램 만들기
 	public static void main(String[] args) {
 		Random rd = new Random();
-
 		int[] arr = new int[6];
 
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < 6; i++) {
 			arr[i] = rd.nextInt(45) + 1;
-			// 중복제거 - 중복이 발생하면 중복이 발생된 i 값을 - 시켜서 다시생성
 			for (int j = 0; j < i; j++) {
 				if (arr[i] == arr[j]) {
 					i--;
@@ -19,7 +15,8 @@ public class Ex21 {
 			} // for
 		} // for
 
-		for (int i : arr) {
+		for (int k = 0; k < arr.length; k++) {
+			int i = arr[k];
 			System.out.println(" 행운의 숫자 : " + i);
 		}
 	}
