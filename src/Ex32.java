a69d2d03f571b9e6b698ad4744c08636a846fe98
rslivepark <146307227+rslivepark@users.noreply.github.com>
2023-10-22 18:30:49 +0900

    Update Ex32.java

--- src/Ex32.java
+++ src/Ex32.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
 
 public class Ex32 {
+//ox 점수계산
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("===채점하기===");
+		String ans = sc.next();
+		String[] arr = ans.split("");
+		int score = 0;
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i].equals("o")) {
+				score++;
+			} else {
+				score = 0;
+			}
+			sum += score;
+
+		}
+		System.out.println(sum);
+	}
 }
