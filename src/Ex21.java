6cf787e77423eca893ce93455fc7b4399c006ced
rslivepark <146307227+rslivepark@users.noreply.github.com>
2023-10-22 18:18:07 +0900

    Add code lines
    코드 재작성

--- src/Ex21.java
+++ src/Ex21.java
@@ -1,23 +1,39 @@
-import java.util.*;
 
-public class Test {
-	// 중복없이 숫자 뽑는 로또 프로그램 만들기
+import java.util.Arrays;
+import java.util.Random;
+
+public class Ex21 {
+//중복 없이 숫자 뽑는 로또 프로그램 만들기 
 	public static void main(String[] args) {
-		Random rd = new Random();
-		int[] arr = new int[6];
+		// 로또 번호를 저장할 배열
+		int[] lottoNumbers = new int[6];
+
+		// 랜덤 숫자 생성을 위한 Random 객체 생성
+		Random random = new Random();
 
 		for (int i = 0; i < 6; i++) {
-			arr[i] = rd.nextInt(45) + 1;
-			for (int j = 0; j < i; j++) {
-				if (arr[i] == arr[j]) {
-					i--;
-				}
-			} // for
-		} // for
+			int randomNumber;
+			do {
+				// 1부터 45 사이의 난수 생성
+				randomNumber = random.nextInt(45) + 1;
+			} while (contains(lottoNumbers, randomNumber)); // 중복 확인
 
-		for (int k = 0; k < arr.length; k++) {
-			int i = arr[k];
-			System.out.println(" 행운의 숫자 : " + i);
+			lottoNumbers[i] = randomNumber;
 		}
+		// 번호를 정렬
+		Arrays.sort(lottoNumbers);
+
+		// 생성된 로또 번호 출력
+		System.out.println("로또 번호: " + Arrays.toString(lottoNumbers));
+	}
+
+	// 배열에서 숫자의 중복을 확인하는 메소드
+	public static boolean contains(int[] array, int number) {
+		for (int value : array) {
+			if (value == number) {
+				return true; // 중복된 숫자가 존재하면 true 반환
+			}
+		}
+		return false; // 중복된 숫자가 없으면 false 반환
 	}
 }
--- src/Ex31.java
+++ src/Ex31.java
@@ -5,13 +5,11 @@ public class Ex31 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("첫번째 숫자 입력>> ");
-		int a = sc.nextInt();
+		int num1 = sc.nextInt();
 		System.out.print("두번째 숫자 입력>> ");
-		int b = sc.nextInt();
+		int num2 = sc.nextInt();
 		
-		System.out.println(a*(b%10));
-		System.out.println(a*((b/10)%10));
-		System.out.println(a*(b/100));
-		System.out.println(a*b);
+		
+		
 	}
 }
