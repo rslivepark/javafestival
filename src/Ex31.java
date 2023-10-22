855c7f5e320551e91422ace99fe0aa89f5753dd8
rslivepark <146307227+rslivepark@users.noreply.github.com>
2023-10-22 18:16:24 +0900

    Update Ex31.java

--- src/Ex31.java
+++ src/Ex31.java
@@ -5,11 +5,13 @@ public class Ex31 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("첫번째 숫자 입력>> ");
-		int num1 = sc.nextInt();
+		int a = sc.nextInt();
 		System.out.print("두번째 숫자 입력>> ");
-		int num2 = sc.nextInt();
+		int b = sc.nextInt();
 		
-		
-		
+		System.out.println(a*(b%10));
+		System.out.println(a*((b/10)%10));
+		System.out.println(a*(b/100));
+		System.out.println(a*b);
 	}
 }
