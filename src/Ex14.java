2c57f94d867ce84a2ba449f7b53827d70e8acfa4
rslivepark <146307227+rslivepark@users.noreply.github.com>
2023-10-22 13:57:29 +0900

    Update Ex14.java
    reduce code lines

--- src/Ex14.java
+++ src/Ex14.java
@@ -1,27 +1,17 @@
 public class Ex14 {
     public static void main(String[] args) {
-        String word1 = "power";
-        String word2 = "test";
+		System.out.println(getMiddle("power"));
 
-        String middle1 = getMiddle(word1);
-        String middle2 = getMiddle(word2);
-
-        System.out.println("가운데 글자(들) (word1): " + middle1);
-        System.out.println("가운데 글자(들) (word2): " + middle2);
-    }
-
-    public static String getMiddle(String word) {
-        int length = word.length();
-
-        if (length % 2 == 0) {
-            // 단어의 길이가 짝수인 경우
-            int mid1 = length / 2 - 1;
-            int mid2 = length / 2;
-            return word.substring(mid1, mid2 + 1);
-        } else {
-            // 단어의 길이가 홀수인 경우
-            int mid = length / 2;
-            return word.substring(mid, mid + 1);
-        }
-    }
+	}
+    
+	public static String getMiddle(String word) {
+		String[] arr = word.split("");
+			
+		if(arr.length%2==0) {
+			return arr[(arr.length/2)-1]+arr[arr.length/2];
+		}
+		else {
+			return arr[arr.length/2];
+		}// if
+	}//getMiddle
 }
